0dea2c8ce324db8b40475a49eb934f79315295f0
jshou <joshuahou@gmail.com>
2012-12-14 21:50:38 -0800

    extracted player to Whale module

--- js/runner.js
+++ js/runner.js
@@ -3,10 +3,6 @@ require.config({
     easel: {
       exports: 'createjs'
     },
-
-    keypress: {
-      exports: 'keypress'
-    },
   },
   paths: {
     easel: 'easeljs-0.5.0.min',
@@ -14,69 +10,15 @@ require.config({
   }
 });
 
-require(['easel', 'keypress'], function(createjs, k) {
+require(['easel', 'whale'], function(createjs, Whale) {
   var canvas = $('.main')[0];
   var stage = new createjs.Stage(canvas);
-  var gravity = -0.1;
 
-  var Player = function(stage) {
-    // player attributes
-    var forward = true;
-    var speed = 1;
-    var color = 'rgba(255,0,0,1)';
-    var size = 20;
-    var jumpVelocity = 5;
-    var lastJump = 0;
 
-    // initialize player shape
-    var shape = new createjs.Shape();
-    shape.graphics.beginFill(color).drawRect(0,0, size, size);
-    stage.addChild(shape);
-    shape.y = stage.canvas.height - size;
-    shape.x = stage.canvas.width / 2 - size / 2;
-
-    k.combo('space', function() {
-      if (!lastJump) {
-        lastJump = 1;
-      }
-    });
-
-    // player's tick function
-    var tick = function() {
-      // handle jump
-      if (lastJump) {
-        var height = jumpVelocity * lastJump + 0.5 * gravity*Math.pow(lastJump, 2);
-        shape.y = canvas.height - size - height;
-        lastJump++;
-
-        if (shape.y > stage.canvas.height - size) {
-          lastJump = 0;
-          shape.y = stage.canvas.height - size;
-        }
-      }
-
-      // bounce back and forth
-      if (shape.x + size >= canvas.width || shape.x < 0) {
-        forward = !forward;
-      }
-
-      if (forward) {
-        shape.x += speed;
-      } else {
-        shape.x -= speed;
-      }
-    };
-
-    return {
-      shape: shape,
-      tick: tick
-    };
-  };
-
-  var player = new Player(stage);
+  var whale = new Whale(canvas, stage);
 
   stage.tick = function() {
-    player.tick();
+    whale.tick();
     this.update();
   };
 
---
+++ js/whale.js
@@ -0,0 +1,74 @@
+require.config({
+  shim: {
+    easel: {
+      exports: 'createjs'
+    },
+
+    keypress: {
+      exports: 'keypress'
+    },
+  },
+  paths: {
+    easel: 'easeljs-0.5.0.min',
+    keypress: 'keypress',
+  }
+});
+
+define(['easel', 'keypress'], function(createjs, k) {
+  var Whale = function(canvas, stage) {
+    // attributes
+    var forward = true;
+    var speed = 1;
+    var color = 'rgba(255,0,0,1)';
+    var size = 20;
+    var jumpVelocity = 5;
+    var gravity = -0.1;
+    var lastJump = 0;
+
+    // initialize shape
+    var shape = new createjs.Shape();
+    shape.graphics.beginFill(color).drawRect(0,0, size, size);
+    stage.addChild(shape);
+    shape.y = stage.canvas.height - size;
+    shape.x = stage.canvas.width / 2 - size / 2;
+
+    k.combo('space', function() {
+      if (!lastJump) {
+        lastJump = 1;
+      }
+    });
+
+    // tick function
+    var tick = function() {
+      // handle jump
+      if (lastJump) {
+        var height = jumpVelocity * lastJump + 0.5 * gravity*Math.pow(lastJump, 2);
+        shape.y = canvas.height - size - height;
+        lastJump++;
+
+        if (shape.y > stage.canvas.height - size) {
+          lastJump = 0;
+          shape.y = stage.canvas.height - size;
+        }
+      }
+
+      // bounce back and forth
+      if (shape.x + size >= canvas.width || shape.x < 0) {
+        forward = !forward;
+      }
+
+      if (forward) {
+        shape.x += speed;
+      } else {
+        shape.x -= speed;
+      }
+    };
+
+    return {
+      shape: shape,
+      tick: tick
+    };
+  };
+
+  return Whale;
+});
