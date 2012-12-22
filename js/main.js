debd4a70a67a5143afb9426917310217d114fec6
jshou <joshuahou@gmail.com>
2012-12-22 01:12:07 -0800

    click start to play again

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
 require(['game'], function(Game) {
+  $('.start').on('click', function() {
+    $('canvas').remove();
+    $('.start').hide();
+    $('.container').append($('<canvas class="main" height="500" width="800"></canvas>'));
+
+    Game(function() {
+      $('.start').show();
+      $('.start').val('Play again');
+    });
+  });
 });
